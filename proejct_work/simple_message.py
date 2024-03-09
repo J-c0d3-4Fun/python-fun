edd2f7e4d4775646dfb938f80706892e40cada43
J-c0d3-4Fun <115200996+J-c0d3-4Fun@users.noreply.github.com>
2024-03-09 09:05:06 -0500

    Delete proejct_work directory

--- proejct_work/simple_message.py
+++
@@ -1,4 +0,0 @@
-#2-1. Simple Message: Assign a message to a variable, and then print that message.
-
-message = "I like to practice Python!"
-print(message)
--- proejct_work/simple_messages.py
+++
@@ -1,6 +0,0 @@
-#2-2. Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
-
-message = "we are practicing changing the variable"
-print(message)
-message = "this is the new variable"
-print(message)
