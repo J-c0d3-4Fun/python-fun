e1fce204d3b385f3899a42b8d84fe86c7fdc9b1f
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-03-08 22:49:58 -0500

    added variables

--- proejct_work/hello_world.py
+++ proejct_work/hello_world.py
@@ -3,3 +3,6 @@ print("Hello Python World!")
 #Variables - Every variable is connected to a value, which is the information associated with that variable
 message = "Hello Python World!"
 print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
