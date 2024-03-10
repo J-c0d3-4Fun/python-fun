3c34a0cfb48c2515f9a5037dfc219a6e7e29fb90
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-03-10 19:26:42 -0400

    numbers overview

---
+++ project_work/numbers.py
@@ -0,0 +1,19 @@
+#try it yourself
+
+# 2-9. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results. You should create four lines that look like this: print(5+3) Your output should be four lines, with the number 8 appearing once on each line. 
+print(4+4)
+print(2*4)
+print(2**3)
+print(12-4)
+print(32/4)
+
+
+#2-10. Favorite Number: Use a variable to represent your favorite number. Then, using that variable, create a message that reveals your favorite number. Print that message.
+favorite_number = 7
+message = f"My favorite number is {favorite_number}!"
+print(message)
+
+
+
+
+
