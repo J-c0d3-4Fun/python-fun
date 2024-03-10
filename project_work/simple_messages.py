e087ce5c80a21d8600736133a9bf036d325d0748
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-03-09 22:50:30 -0500

    strings try it yourself exercises

--- project_work/simple_messages.py
+++ project_work/simple_messages.py
@@ -1,3 +1,5 @@
+#Try it yourself
+
 #2-2. Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
 
 message = "we are practicing changing the variable"
