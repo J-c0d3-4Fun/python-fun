4225f31fab6ba1929dd3067ef936f8ab0aaaaa4a
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-04-01 22:31:32 -0400

    Chapter 6

--- project_work/conditions-chpt5-try.py
+++ project_work/conditions-chpt5-try.py
@@ -278,6 +278,12 @@ else:
 current_users = ['jaden', 'will', 'jim', 'dave', 'tom']
 new_users = ['jaden', 'tammy', 'timmy', 'josh', 'tom']
 
+for users in new_users:
+      if users in current_users:
+            print("You will need to enter a new username!")
+        
+      else: 
+            print(f"That username is available!")
 
 
 
@@ -289,5 +295,16 @@ new_users = ['jaden', 'tammy', 'timmy', 'josh', 'tom']
 # for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th
 # 8th 9th", and each result should be on a separate line.
 
+ordinal = [1, 2, 3, 4, 5, 6, 7 ,8 ,9]
+
+for numbers in ordinal:
+    if numbers == 1:
+        print(f"1st")
+    elif numbers == 2:
+        print(f"2nd")
+    elif numbers == 3:
+        print(f"3rd")
+    else:
+          print(f"{numbers}th")
 
 
