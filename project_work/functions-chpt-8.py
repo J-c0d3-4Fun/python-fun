daab34d1a0a0041c683ee037af4a8c31b8bed473
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-05-15 00:06:35 -0400

    chapter 8

--- project_work/functions-chpt-8.py
+++ project_work/functions-chpt-8.py
@@ -342,12 +342,19 @@ while True:
     formatted_name = get_formatted_name(f_name, l_name)
     print(f"Hello, {formatted_name}")
 
+# Passing a list
+# You’ll often find it useful to pass a list to a function, whether it’s a list of names, numbers, or more complex objects, such as dictionaries. 
+# When you pass a list to a function, the function gets direct access to the contents of the list. 
+# Let’s use functions to make working with lists more efficient.
 
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
 
-
-
-
-
+usernames = ['hannah','ty','margot']
+greet_users(usernames)
 
 
 
