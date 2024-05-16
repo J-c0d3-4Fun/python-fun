b033238c4705873017b0ee8297cab08844315506
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-05-15 22:06:13 -0400

    chapter 8

--- project_work/functions-chpt-8.py
+++ project_work/functions-chpt-8.py
@@ -357,5 +357,43 @@ usernames = ['hannah','ty','margot']
 greet_users(usernames)
 
 
+# Modifying a List in a Function
+# When you pass a list to a function, the function can modify the list. Any
+# changes made to the list inside the function’s body are permanent, allowing
+# you to work efficiently even when you’re dealing with large amounts of data.
+
+# Start with some designs that need to be printed.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+#Simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design)
+
+#Display all completed models.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+
+
+# This program starts with a list of designs that need to be printed and
+# an empty list called completed_models that each design will be moved to after
+# it has been printed. As long as designs remain in unprinted_designs, the while
+# loop simulates printing each design by removing a design from the end of
+# the list, storing it in current_design, and displaying a message that the current
+# design is being printed. It then adds the design to the list of completed
+# models. When the loop is finished running, a list of the designs that have
+# been printed is displayed
+
+
+
+
+
+
+
+
 
 
