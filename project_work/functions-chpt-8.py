3755f6ac54da98f833912e1715a81d0d4889b3fd
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-05-26 22:21:10 -0400

    added from chapter 8

--- project_work/functions-chpt-8.py
+++ project_work/functions-chpt-8.py
@@ -450,12 +450,31 @@ function_name(list_name[:])
 # large lists.
 
 
+# Passing am Arbitrary Number of Arguments
+
+def make_pizza(*toppings):
+    """Print the list of toppings tht have been requested."""
+    print(toppings)
+
+make_pizza('pepporoni')
+make_pizza('mushrooms', 'green peppers', 'extrra cheese')
+
+# The asterisk in the parameter name *toppings tells Python to make a tuple called toppings, 
+# containing all the values this function receives. 
+# The print() call in the function body produces output showing that Python can handle a function call with one value and 
+# a call with three values. It treats the different calls similarly. 
+# Note that Python packs the arguments into a tuple, even if the function receives only one value
 
 
 
+# Now we can replace the print() call with a loop that runs through the list of toppings and describes the pizza being ordered
+def make_pizza(*toppings):
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
 
-
-
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 
 
