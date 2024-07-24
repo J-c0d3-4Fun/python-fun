b7195e260fa30a72f207ba532c0bcbfd351c8db6
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-07-23 21:55:09 -0400

    chapter 9 update

--- try-it-yourself-chpt9.py
+++ try-it-yourself-chpt9.py
@@ -170,9 +170,94 @@ class Users:
         self.login_attempts += 1
         print(f"You've attempted {self.login_attempts} number of logins")
 
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 user_1 = Users("John","Doe", 55, "5'9'", "black" )
 user_1.describe_user()
 user_1.greet_user()
-user_1.increment_login_attempts(1)
-user_1.increment_login_attempts(1)
-user_1.increment_login_attempts(1)
+user_1.increment_login_attempts()
+user_1.increment_login_attempts()
+user_1.increment_login_attempts()
+user_1.reset_login_attempts()
+user_1.increment_login_attempts()
+user_1.increment_login_attempts()
+user_1.increment_login_attempts()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 9-6. Ice Cream Stand: An ice cream stand is a specific kind of restaurant. Write
+# a class called IceCreamStand that inherits from the Restaurant class you wrote in
+# Exercise 9-1 (page 162) or Exercise 9-4 (page 166). Either version of the class
+# will work; just pick the one you like better. Add an attribute called flavors that
+# stores a list of ice cream flavors. Write a method that displays these flavors.
+# Create an instance of IceCreamStand, and call this method.
+
+
+class Restaurant:
+    """describing a restaurant"""
+
+    def __init__(self,restaurant_name, cuisine_type, flavors):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.flavors = flavors
+
+    def describe_restaurant(self):
+        print(f"The name of the restaraunt is {self.restaurant_name}")
+        print(f"\n The type of food the restaurant serves is {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open for business!")
+
+
+class IceCreamStand(Restaurant):
+    """Initializes attributes from the parent class restaraunt. 
+    Then allows you to choose your favorite Ice Cream flavor.
+    """
+
+    def __init__(self, restaurant_name, cuisine_type, flavors):
+        super().__init__(restaurant_name, cuisine_type, flavors)
+        self.flavors = flavors
+
+    def favorite(self):
+        """describes the Ice cream flavor"""
+        print(f"my favorite flavor of ice cream is {self.flavors}")
+
+icecream = IceCreamStand("ice day breexe", "cream", "vanilla")
+print(icecream.favorite)
+
+# 9-7. Admin: An administrator is a special kind of user. Write a class called
+# Admin that inherits from the User class you wrote in Exercise 9-3 (page 162)
+# or Exercise 9-5 (page 167). Add an attribute, privileges, that stores a list of
+# strings like "can add post", "can delete post", "can ban user", and so on.
+# Write a method called show_privileges() that lists the administrator’s set of
+# privileges. Create an instance of Admin, and call your method.
+
+
+
+# 9-8. Privileges: Write a separate Privileges class. The class should have one
+# attribute, privileges, that stores a list of strings as described in Exercise 9-7.
+# Move the show_privileges() method to this class. Make a Privileges instance
+# as an attribute in the Admin class. Create a new instance of Admin and use your
+# method to show its privileges.
+
+
+
+# 9-9. Battery Upgrade: Use the final version of electric_car.py from this section.
+# Add a method to the Battery class called upgrade_battery(). This method
+# should check the battery size and set the capacity to 65 if it isn’t already. Make
+# an electric car with a default battery size, call get_range() once, and then
+# call get_range() a second time after upgrading the battery. You should see an
+# increase in the car’s range.
