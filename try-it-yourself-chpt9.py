987b292e9b46bc45fa3130acee05d5860e3da33c
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-07-25 22:34:50 -0400

    chapter 9 update

--- try-it-yourself-chpt9.py
+++ try-it-yourself-chpt9.py
@@ -242,6 +242,7 @@ icecream.favorite()
 
 
 
+
 # 9-7. Admin: An administrator is a special kind of user. Write a class called
 # Admin that inherits from the User class you wrote in Exercise 9-3 (page 162)
 # or Exercise 9-5 (page 167). Add an attribute, privileges, that stores a list of
@@ -272,10 +273,16 @@ class Admin(Users):
 
     def __init__(self, first_name, last_name, age, height, hair_color):
         super().__init__(first_name, last_name, age, height, hair_color)
-        self.priviliges = " "
+        self.privileges = ["can add post", "can delete post"]
 
     def show_privileges(self):
-        
+        print(f"The admin {self.user} current privileges are listed below")
+        for rights in self.privileges:
+            print(f"- {rights}")
+
+user_1 = Admin("John","Doe", 55, "5'9'", "black")
+user_1.show_privileges()
+
 
 
 # 9-8. Privileges: Write a separate Privileges class. The class should have one
@@ -283,6 +290,47 @@ class Admin(Users):
 # Move the show_privileges() method to this class. Make a Privileges instance
 # as an attribute in the Admin class. Create a new instance of Admin and use your
 # method to show its privileges.
+
+
+class Users:
+    """Describing a user using a profile"""
+
+    def __init__(self,first_name,last_name,age,height,hair_color):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.height = height
+        self.hair_color = hair_color
+        self.user = f"{self.first_name} {self.last_name}"
+
+    def describe_user(self):
+        print(f"This is the current profile for the user {self.first_name} {self.last_name}")
+        print(f"{self.first_name}'s current age is {self.age} and their height is {self.height}")
+        print(f"Lastly {self.first_name}'s hair color is {self.hair_color}")
+
+    def greet_user(self):
+        print(f"Hello {self.user} welcome to the team!")
+
+class Privileges:
+    def __init__(self):
+        self.privileges = ["can add post", "can delete post"] 
+
+    def show_privileges(self):
+            print(f"The admin's current privileges are listed below")
+            for rights in self.privileges :
+                print(f"- {rights}")
+
+class Admin(Users):
+
+    def __init__(self, first_name, last_name, age, height, hair_color):
+        super().__init__(first_name, last_name, age, height, hair_color)
+        self.privileges = Privileges()
+
+
+administrator = Admin("John","Doe", 55, "5'9'", "black")
+administrator.describe_user()
+administrator.privileges.show_privileges()
+
 
 
 
