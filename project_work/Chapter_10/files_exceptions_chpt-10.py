9d46fff5256b479adc3aa3af67bf7625a04e9bde
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-08-26 22:30:05 -0400

    chapter 10

--- project_work/Chapter_10/files_exceptions_chpt-10.py
+++ project_work/Chapter_10/files_exceptions_chpt-10.py
@@ -184,18 +184,52 @@ print(len(pi_string))
 # the resources available at https://ehmatthes.github.io/pcc_3e.
 
 
+# Writing to a File 
 
+# Writing a Single Line 
 
+# Once you have a path defined, you can write to a file using the write_text() method.
 
+from pathlib import Path
 
+path = Path('programming')
+path.write_text("I love programming.")
 
+# The write_text() method takes a single argument: the string that you want to write to the file. This program has no terminal output, 
+# but if you open the file programming.txt, you’ll see one line.
 
+# NOTE Python can only write strings to a text file. If you want to store numerical data
+# in a text file, you’ll have to convert the data to string format first using the str()
+# function
 
+# Writing Multiple Lines 
 
+# The write_text() method does a few things behind the scenes. If the file that
+# path points to doesn’t exist, it creates that file. Also, after writing the string
+# to the file, it makes sure the file is closed properly. Files that aren’t closed
+# properly can lead to missing or corrupted data.
+# To write more than one line to a file, you need to build a string containing
+# the entire contents of the file, and then call write_text() with that string.
 
+from pathlib import Path
 
+contents = "I love programming. \n"
+contents += "I love creating new games. \n"
+contents += "I also love working with data \n"
 
+path = Path('programming.txt')
+path.write_text(contents)
 
+# We define a variable called contents that will hold the entire contents of
+# the file. On the next line, we use the += operator to add to this string. You
+# can do this as many times as you need, to build strings of any length. In this
+# case we include newline characters at the end of each line, to make sure
+# each statement appears on its own line.
+
+# NOTE Be careful when calling write_text() on a path object. If the file already exists,
+# write_text() will erase the current contents of the file and write new contents
+# to the file. Later in this chapter, you’ll learn to check whether a file exists using
+# pathlib.
 
 
 
--- project_work/Chapter_10/try_it_yourself.py
+++ project_work/Chapter_10/try_it_yourself.py
@@ -19,7 +19,74 @@ print(contents)
 # 'I really like cats.'
 # Read in each line from the file you just created, learning_python.txt, and replace the word Python with the name of another language, such as C. Print each modified line to the screen.
 
+from pathlib import Path
+
+path = Path('learning_python.txt')
+contents = path.read_text()
+changed = contents.replace('Python','Java')
+print(changed)
+
+
+
 
 # 10-3. Simpler Code: The program file_reader.py in this section uses a temporary variable, lines, to show how splitlines() works. You can skip the temporary variable and loop directly over the list that splitlines() returns:
 # for line in contents.splitlines():
 # Remove the temporary variable from each of the programs in this section, to make them more concise.
+
+# file_reader.py
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+for line in contents.splitlines():
+    print(line)
+
+# pi_string.py
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+pi_string = ''
+for line in contents.splitlines():
+    pi_string += line
+
+print(pi_string)
+print(len(pi_string))
+
+# pi_string.py part 2
+from pathlib import Path
+
+path = Path('pi_million_digits.txt')
+contents = path.read_text()
+
+pi_string = ''
+for line in contents.splitlines():
+    pi_string += line.lstrip()
+
+print(f"{pi_string[:52]}....")
+print(len(pi_string))
+
+
+
+
+# 10-4. Guest: Write a program that prompts the user for their name. When they respond, write their name to a file called guest.txt.
+# 10-5. Guest Book: Write a while loop that prompts users for their name. Collect all the names that are entered, 
+# and then write these names to a file called guest_book.txt. Make sure each entry appears on a new line in the file.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
