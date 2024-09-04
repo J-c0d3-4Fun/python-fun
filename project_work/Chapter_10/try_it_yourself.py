52395e8c420da0214daea3b9909d239810b656e1
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-09-03 22:56:14 -0400

    adding to chapter 10 stuff

--- project_work/Chapter_10/try_it_yourself.py
+++ project_work/Chapter_10/try_it_yourself.py
@@ -101,8 +101,20 @@ path.write_text(contents)
 # 10-6. Addition: One common problem when prompting for numerical input occurs when people provide text instead of numbers. When you try to convert the input to an int, you’ll get a ValueError. 
 # Write a program that prompts for two numbers. Add them together and print the result. Catch the ValueError if either input value is not a number, and print a friendly error message. 
 # Test your program by entering two numbers and then by entering some text instead of a number.
+
+print("I can add numbers for you!")
  
- 
+def adding():
+    try:
+        first = input("What is the first number you want to add?: ")
+        second = input("What is the second number you want to add?: ")
+        addition = int(first) + int(second)
+    except ValueError:
+        print("That is not an interger!")
+    else:
+        print(f"The answer is {addition}!")
+    
+adding()
 
 
 # 10-7. Addition Calculator: Wrap your code from Exercise 10-6 in a while loop so the user can continue entering numbers, even if they make a mistake and enter text instead of a number.
