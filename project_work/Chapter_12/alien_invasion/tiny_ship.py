9be047bc8c83dcb3cf942466005509c8b7345681
J-c0d3-4Fun <jabbrown001@gmail.com>
2024-12-27 09:33:48 -0500

    added some code for the ship , need to add the screen color

--- project_work/Chapter_12/alien_invasion/tiny_ship.py
+++ project_work/Chapter_12/alien_invasion/tiny_ship.py
@@ -7,7 +7,7 @@ class tship:
     def __init__(self,tship):
         self.screen = tship.screen
         self.screen_rect = tship.screen.get_rect()
-        self.image = pygame.image.load('images/tiny_ship.bmp')
+        self.image = pygame.image.load('/Users/jbrown/python-fun/project_work/Chapter_12/alien_invasion/images/tiny.bmp')
         self.rect = self.image.get_rect()
         self.rect.bottomleft = self.screen_rect.bottomleft
         self.moving_up = False
@@ -19,3 +19,7 @@ class tship:
             self.rect.y -= 1
         elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.rect.y += 1
+    
+    def blitme(self):
+        """This will draw the tiny ship on the screen at its current location"""
+        self.screen.blit(self.image,self.rect)
